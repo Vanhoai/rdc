967046ddd103719fa074ccd48a0fc25a42486283
Aurora Study <hardy251223@gmail.com>
2024-08-28 18:18:38 +0700

    Share workspace dependancy

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -8,7 +8,7 @@ mod api;
     about = "stringer - a simple CLI to transform and inspect strings",
     long_about = "stringer is a super fancy CLI (kidding) One can use stringer to modify or inspect strings straight from the terminal"
 )]
-pub struct Cli {
+pub struct RdcCli {
     #[command(subcommand)]
     command: Option<Commands>,
 }
@@ -21,20 +21,18 @@ enum Commands {
 
 #[derive(Args)]
 struct Reverse {
-    /// The string to reverse
     string: Option<String>,
 }
 
 #[derive(Args)]
 struct Inspect {
-    /// The string to inspect
     string: Option<String>,
     #[arg(short = 'd', long = "digits")]
     only_digits: bool,
 }
 
 fn main() {
-    let cli = Cli::parse();
+    let cli = RdcCli::parse();
 
     match &cli.command {
         Some(Commands::Reverse(name)) => match name.string {
